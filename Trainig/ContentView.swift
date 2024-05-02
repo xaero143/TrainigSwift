7b5ad688e406cf0eb6e2d1b5b799bda4dd070c04
Serghei <corostin@gmail.com>
2024-05-02 22:36:44 +0300

    Initial Commit

---
+++ Trainig/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Trainig
+//
+//  Created by Serghei on 26.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View 
+{
+    var body: some View 
+    {
+        VStack 
+        {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                .font(.largeTitle)
+                .fontWeight(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
+                .blendMode(/*@START_MENU_TOKEN@*/.darken/*@END_MENU_TOKEN@*/)
+                .menuOrder(/*@START_MENU_TOKEN@*/.priority/*@END_MENU_TOKEN@*/)
+            
+
+            
+        }
+
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trainig/TrainigApp.swift
@@ -0,0 +1,20 @@
+//
+//  TrainigApp.swift
+//  Trainig
+//
+//  Created by Serghei on 26.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrainigApp: App 
+{
+    var body: some Scene 
+    {
+        WindowGroup 
+        {
+            ContentView()
+        }
+    }
+}
